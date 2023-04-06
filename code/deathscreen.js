c2b32672cd2989b4c6655403c76c458691a31ee1
GamySide <nataelforget@gmail.com>
2023-04-06 15:21:40 +0200

    .

--- code/deathscreen.js
+++ code/deathscreen.js
@@ -7,13 +7,30 @@ class Deathscreen extends Phaser.Scene {
     }
     create() {
         this.add.image(320, 180, 'deathscreen');
-        this.clavier = this.input.keyboard.addKeys('A,Z,E,R,ENTER,ESCAPE');
+        this.clavier = this.input.keyboard.addKeys('A,Z,E,R,ENTER,ESC');
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.pad = {
+            leftStick: { x: 0, y: 0 },
+            rightStick: { x: 0, y: 0 },
+            A: false,
+            B: false,
+            X: false,
+            Y: false,
+            L1: 0,
+            L2: 0,
+            R1: 0,
+            R2: 0,
+            right: false,
+            left: false,
+            up: false,
+            down: false,
+        }
     }
     update() {
         if (this.clavier.ENTER.isDown) {
             location.reload();
         }
-        if (this.clavier.A.isDown) {
+        if (this.clavier.ESC.isDown) {
             window.close();
         }
     }
--- code/titlescene.js
+++ code/titlescene.js
@@ -30,10 +30,38 @@ class Titlescene extends Phaser.Scene {
                 hp : 10,
             });
         });
+        this.clavier = this.input.keyboard.addKeys('A,Z,E,R,ENTER,ESC');
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.pad = {
+            leftStick: { x: 0, y: 0 },
+            rightStick: { x: 0, y: 0 },
+            A: false,
+            B: false,
+            X: false,
+            Y: false,
+            L1: 0,
+            L2: 0,
+            R1: 0,
+            R2: 0,
+            right: false,
+            left: false,
+            up: false,
+            down: false,
+        }
 
     }
     update() {
-        
+        if(this.clavier.ENTER.isDown) {
+            this.scene.start('maisonPP',{
+                x : 13*16,
+                y :4*32,
+                hpmax : 10,
+                hp : 10,
+            });
+        }
+        if (this.clavier.ESC.isDown) {
+            window.close();
+        }
     }
 
 }
